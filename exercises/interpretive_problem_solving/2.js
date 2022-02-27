d0d436f1aef0477edb5eea5ba0c8c1bd5fc47928
Chelsea Saunders <c_chilcoat@msn.com>
2022-02-27 14:23:06 -0800

    working through interpretive problem solving problems

--- exercises/interpretive_problem_solving/2.js
+++ exercises/interpretive_problem_solving/2.js
@@ -4,7 +4,57 @@
 // n is an odd integer supplied as an argument to the function. You may assume
 // that the argument will always be an odd integer.
 
+/*
+Take a number (n)
+- output n rows of *'s and ' 's
+- center row will have n *'s and no spaces
+- numbers up to center row will start with one star preceded by n / 2 .floor
+  spaces
+- each row counting up to the ceter row will have one less space and one more
+  star (spaces always precede stars)
+- each row after center will follow the reverse pattern
+
+DATA:
+input: positive integer
+output: string
+processing: string?
+
+ALGO:
+create variable center, set to Math.ceil(number / 2);
+create variable starCounter, set to 1;
+create variable spaceCounter, set to center - 1;
+
+create for loop, let row = 1; keep while row <= input number, add 1 each time;
+if row === center
+console.log inputNumber * '*';
+else output spaces.repeat spaceCounter + stars.repeat star counter;
+if row < center spaces -= 1, stars += 2
+else center spaces += 1, stars -= 2;
+*/
+
 // Examples:
+
+function diamond(n) {
+  let center = Math.ceil(n / 2);
+  let starCounter = 1;
+  let spaceCounter = center - 1;
+
+  for (let row = 1; row <= n; row += 1) {
+    if (row === center) {
+      console.log('*'.repeat(n));
+    } else {
+      console.log(' '.repeat(spaceCounter) + '*'.repeat(starCounter));
+    }
+
+    if (row < center) {
+      starCounter += 2;
+      spaceCounter -=1;
+    } else {
+      starCounter -= 2;
+      spaceCounter += 1;
+    }
+  }
+}
 
 diamond(1);
 // logs
--- exercises/interpretive_problem_solving/4.js
+++ exercises/interpretive_problem_solving/4.js
@@ -14,22 +14,123 @@
 // same letter case as the original letter. If the key value for shifting
 // exceeds the length of the alphabet, it wraps around from the beginning.
 
+/* PROBLEM:
+Create a program that takes text and an integer, and shifts every letter value
+  in the text by the integer number based on the alphabetical position of the
+  letter, and keeping the letter case in tact.
+
+RULES:
+- keep the letter case in tact (so if a letter is lowercase, replacement shouls
+  be lowercase)
+- if adding the letter shift value to the current letter value position is
+  greater than 26 (then start back at zero, with exess number, so if you had 28,
+  thats 2 more than max alpha value, so you would land on 'B', the second alpha
+  charatcter)
+- ignore non-letter values
+
+Questions:
+- Do we have to validate input? --make sure text is strng; make sure
+  shift number is intgeter
+- What to do with negative shift (work the shift in reverse or treat as ivalid)
+- What to do with empty strings? --> presumably return an empty string
+
+
+DATA:
+input: string, integer
+output: rotated string
+processing:
+- array to hold alpha values
+
+
+ALGO:
+create constant; array with alphabetic charaters in order, lowecase
+
+create function
+*validate input here
+initialize encrypted to empty string;
+
+format shift numvber
+  helper function:
+    if absolute value of number is greater than or equal to 26, reassign to
+      remainder of number / 26
+    if that number is negative, reassign to 26 - number;
+    return number
+if number is 0, return original string
+
+for loop, with 0 as initial value; contineue as long as shoters that text length
+  and incriment by 1
+  currentChar = text.charAt i
+  if currentChar.match(^[a-z]i), concat currectChar to encrypted
+  else
+    - create helper method, replace current letter, use current letter and shift
+      number
+    HELPER FUNCTION
+      set current charachter position to alph.indexOf(currentLetter.toLowerCase)
+      - add shift number to current position
+         if that value is greater than alpha.length
+         reassign to that number - alpha.length
+      let newletter = array[position above]
+      if initial letter === initialLetter.toUpperCAse, return
+      newletterToUpperCAse, otherwise return new letter
+    append this new charahter to encrypted string
+
+  return encrypted string
+*/
+
+
 // Examples:
 
+const alpha = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+                'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y',
+                'z'];
+
+function caesarEncrypt(text, shiftNum) {
+  shiftNum = simplifyShiftNum(shiftNum)
+  if (shiftNum === 0 || text.length === 0) return text;
+  let encrypted = '';
+
+  for (let i = 0; i < text.length; i += 1) {
+    let currentChar = text.charAt(i);
+    if (!alpha.includes(currentChar.toLowerCase())) {
+      encrypted += currentChar;
+    } else {
+      encrypted += encodeLetter(currentChar, shiftNum);
+    }
+  }
+  return encrypted;
+}
+
+function simplifyShiftNum(num) {
+  if (Math.abs(num) >= 26) num = num % 26;
+  if (num < 0) num = 26 - num;
+  return num;
+}
+
+function encodeLetter(letter, shift) {
+  let charPosition = alpha.indexOf(letter.toLowerCase());
+  charPosition += shift;
+  if (charPosition >= alpha.length) charPosition -= alpha.length;
+  if (letter === letter.toLowerCase()) {
+    return alpha[charPosition];
+  } else {
+    return alpha[charPosition].toUpperCase();
+  }
+}
+
 // simple shift
-caesarEncrypt('A', 0);       // "A"
-caesarEncrypt('A', 3);       // "D"
+console.log(caesarEncrypt('A', 0));       // "A"
+console.log(caesarEncrypt('A', 3));       // "D"
 
 // wrap around
-caesarEncrypt('y', 5);       // "d"
-caesarEncrypt('a', 47);      // "v"
+console.log(caesarEncrypt('y', 5));  //z, a, b, c, =>     // "d"
+console.log(caesarEncrypt('a', 47));  // 47 - 26; 21 * remainder of 47 and 26  // "v"
 
 // all letters
-caesarEncrypt('ABCDEFGHIJKLMNOPQRSTUVWXYZ', 25);
+console.log(caesarEncrypt('ABCDEFGHIJKLMNOPQRSTUVWXYZ', 25));
 // "ZABCDEFGHIJKLMNOPQRSTUVWXY"
-caesarEncrypt('The quick brown fox jumps over the lazy dog!', 5);
+console.log(caesarEncrypt('The quick brown fox jumps over the lazy dog!', 5));
 // "Ymj vznhp gwtbs ktc ozrux tajw ymj qfed itl!"
 
 // many non-letters
-caesarEncrypt('There are, as you can see, many punctuations. Right?; Wrong?', 2);
+console.log(caesarEncrypt('There are, as you can see, many punctuations. Right?; Wrong?', 2));
 // "Vjgtg ctg, cu aqw ecp ugg, ocpa rwpevwcvkqpu. Tkijv?; Ytqpi?"
