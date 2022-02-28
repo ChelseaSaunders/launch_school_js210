03e12ea4b197eb95e7160cf270af36ea73185100
Chelsea Saunders <c_chilcoat@msn.com>
2022-02-28 12:47:53 -0800

    worked thruogh another interpretive prolem solving problem

--- exercises/interpretive_problem_solving/5.js
+++ exercises/interpretive_problem_solving/5.js
@@ -34,3 +34,104 @@
 // For a quick lookup of a ciphertext per character, you may consult this tabula
 // recta. Each row of the table corresponds to a Caesar Cipher encryption using
 // the columns' character letter as a shift value.
+
+/*
+Problem:
+Take text, and a word.  Use the charachters in the word to encrypt the text.
+  Each character in the word corresponds to its index position (0-25) in the
+  alphabet.  Shift characters in the text for each value in the word, when the
+  last value in the word is used, start over again (see example) until the
+  text has been converted.
+
+- Rules:
+  - Shift is case insensensitive, BUT encypted string should retain original
+    case (if letter was uppercase before encryption it should be upperase after)
+  - Non alphabetical charachters should be left in place as-is, BUT should not
+    get counted in the shift pattern (eg. if the last alph char was on the shift
+    position 1, and then there was a non alpha char, the next alpha would still
+    use the next shift position, which is 2)
+
+Questions:
+- Validate input, what to do with invalid input
+- What to do with empty strings--both as text or as shift
+- What to do if shift string is longer than text
+- What to do with text strings that contain only non-alpha char
+
+DATA:
+Input: string, string
+Output string:
+Storing key, use string (char index positions)
+data manipulation: ustring
+
+ALOGO:
+create const all alpha chars *lowercase*
+
+convert shift string into array of number shift values
+  HELPER FUNCTION:
+    - split string into array of chars
+    - map over char array using index Of from alpha string
+initialize empty string to encrypted
+create shiftCount, assign to 0
+Use a for loop to iterate through string - i= 0, i < text length, + 1
+if text[i].match (/[^a-z]/i) add char to encrypted
+else
+helper function to encrypt char, and add to string
+  HELPER FUNCTION currentChar, encryptArray[shiftCount]:
+    - get currentChar index pos from alpha, using alpha indexOf and lowercase c
+      char
+    - add encrypt value
+      - if greater than or equal to the length of alpha, then reassign to new
+        value - alpha length
+    - assign encrypt to alpha[posiotn]
+    - if the orig char was uppercase reassign enncrypt value to .toUpperCase()
+
+reassign shiftCount (use helper function)
+  HELPER FUNCTION:
+    - reassign shiftCount to shiftCount + 1
+    - if that is === to the length of the encrypt array, reassign to 0
+return encrypt string
+*/
+
+const alpha = 'abcdefghijklmnopqrstuvwxyz';
+
+function vigenereCypher(text, shift) {
+  let encryptKey = generateEncryptKey(shift);
+  let encrypted = '';
+  let shiftCount = 0;
+
+  for (let i = 0; i < text.length; i += 1) {
+    let currentChar = text.charAt(i);
+    if (currentChar.match(/[^a-z]/i)) {
+      encrypted += currentChar;
+    } else {
+      encrypted += encryptChar(currentChar, encryptKey[shiftCount]);
+      shiftCount = calculateShiftCount(shiftCount, encryptKey);
+    }
+  }
+
+  return encrypted;
+}
+
+function generateEncryptKey(shiftString) {
+  return shiftString.split('').map((char) => alpha.indexOf(char));
+}
+
+function encryptChar(char, shiftValue) {
+  let position = alpha.indexOf(char.toLowerCase());
+  position += shiftValue;
+  if (position >= alpha.length) position -= alpha.length;
+  let encryptChar = alpha[position];
+  return char === char.toUpperCase() ? encryptChar.toUpperCase() : encryptChar;
+}
+
+function calculateShiftCount(count, keyArr) {
+  count += 1;
+  return count >= keyArr.length ? count - keyArr.length : count;
+}
+
+console.log(vigenereCypher("Pineapples don't go on pizzas!", 'meat')); // Bmnxmtpeqw dhz'x gh ar pbldal!
+console.log(vigenereCypher('', '')); // ''
+// console.log(vigenereCypher("Pineapples don't go on pizzas!", ''));  // Pineapples don't go on pizzas!
+// console.log(vigenereCypher('', 'meat')); // ''
+console.log(vigenereCypher('abc', 'abcd')); // 0, 1, 2; ace
+console.log(vigenereCypher('123', 'ab'));
