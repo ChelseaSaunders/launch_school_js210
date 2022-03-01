0ce5c86280171ad7bd6bc0aee75c26238d9b7849
Chelsea Saunders <c_chilcoat@msn.com>
2022-03-01 15:14:30 -0800

    working through advanced exercises

--- exercises/advanced_1/5.js
+++ exercises/advanced_1/5.js
@@ -8,9 +8,41 @@
 
 // Your solution should not mutate the input arrays.
 
+/*
+rules:
+2 arrays return one new array
+new array contains values in order from lowest to highest (not just alternating
+  values between the two arrays)
+empty arrays are ignored--copy of non-empty array is returned
+
+
+*/
+
 // Examples:
 
-merge([1, 5, 9], [2, 6, 8]);      // [1, 2, 5, 6, 8, 9]
-merge([1, 1, 3], [2, 2]);         // [1, 1, 2, 2, 3]
-merge([], [1, 4, 5]);             // [1, 4, 5]
-merge([1, 4, 5], []);             // [1, 4, 5]
+function merge(arr1, arr2) {
+  let merged = [];
+  let arr1Idx = 0;
+  let arr2Idx = 0;
+
+  while (arr1Idx <= arr1.length && arr2Idx <= arr2.length) {
+    if (arr1[arr1Idx] === undefined && arr2[arr2Idx] === undefined) {
+      break;
+    } else if (arr2[arr2Idx] === undefined || arr1[arr1Idx] <= arr2[arr2Idx]) {
+      merged.push(arr1[arr1Idx]);
+      arr1Idx += 1;
+
+    } else {
+      merged.push(arr2[arr2Idx]);
+      arr2Idx += 1;
+    }
+  }
+
+  return merged;
+}
+
+console.log(merge([1, 5, 9], [2, 6, 8]));      // [1, 2, 5, 6, 8, 9]
+console.log(merge([1, 1, 3], [2, 2]));          // [1, 1, 2, 2, 3]
+console.log(merge([2, 2], [1, 1, 3]));          // [1, 1, 2, 2, 3]
+console.log(merge([], [1, 4, 5]));             // [1, 4, 5]
+console.log(merge([1, 4, 5], []));             // [1, 4, 5]
