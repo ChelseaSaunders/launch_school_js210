6c4376d0eaad1bb5828b1e41361bd48ac101f557
Chelsea Saunders <c_chilcoat@msn.com>
2022-03-01 12:51:13 -0800

    working through advanced exercises

--- exercises/advanced_1/1.js
+++ exercises/advanced_1/1.js
@@ -23,29 +23,51 @@
 // strings. The words in quotes come from the list of texts and it is the
 // madlibs function that puts them in.
 
+// These examples use the following list of replacement texts:
+const adjectives = ['quick', 'lazy', 'sleepy', 'noisy', 'hungry'];
+const nouns =  ['fox', 'dog', 'head', 'leg', 'tail', 'cat'];
+const verbs = ['jumps', 'lifts', 'bites', 'licks', 'pats'];
+const adverbs = ['easily', 'lazily', 'noisily', 'excitedly'];
+
+let template1 = `The "adjective" brown "noun" "adjective" "verb" the `+
+                `"adjective" "noun", who "adverb" "verb" his "noun" and `+
+                `looks around.`
+
+let template2 = `The "noun" "verb" the "noun"'s "noun".`
+
 function madlibs(template) {
-  // ...
+  template = replaceWords(template, 'noun', nouns);
+  template = replaceWords(template, 'adjective', adjectives);
+  template = replaceWords(template, 'adverb', adverbs);
+  template = replaceWords(template, 'verb', verbs);
+
+  return template;
 }
 
-// These examples use the following list of replacement texts:
-adjectives: quick lazy sleepy noisy hungry
-nouns: fox dog head leg tail cat
-verbs: jumps lifts bites licks pats
-adverbs: easily lazily noisily excitedly
-------
+function replaceWords(madlibTemplate, wordType, wordTypeArray) {
+  let regEx = new RegExp(wordType, 'i');
+  while (madlibTemplate.match(regEx) !== null) {
+    let randomWord = randomElement(wordTypeArray);
+    madlibTemplate = madlibTemplate.replace(regEx, randomWord);
+  }
+  return madlibTemplate;
+}
+function randomElement(array) {
+  let randomIndex = Math.floor(Math.random() * array.length);
+  return array[randomIndex];
+}
 
-madlibs(template1);
+console.log(madlibs(template1));
 // The "sleepy" brown "cat" "noisily"
 // "licks" the "sleepy" yellow
 // "dog", who "lazily" "licks" his
 // "tail" and looks around.
 
-madlibs(template1);
+console.log(madlibs(template1));
 // The "hungry" brown "cat" "lazily"
 // "licks" the "noisy" yellow
 // "dog", who "lazily" "licks" his
 // "leg" and looks around.
 
-madlibs(template2);      // The "fox" "bites" the "dog"'s "tail".
-
-madlibs(template2);      // The "cat" "pats" the "cat"'s "head".
+console.log(madlibs(template2));      // The "fox" "bites" the "dog"'s "tail".
+console.log(madlibs(template2));      // The "cat" "pats" the "cat"'s "head".
