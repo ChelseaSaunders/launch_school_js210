81746b30ec63198926dd0f92ee10595746057eea
Chelsea Saunders <c_chilcoat@msn.com>
2022-03-02 13:30:49 -0800

    finished all advanced problems except 6

--- exercises/advanced_1/7.js
+++ exercises/advanced_1/7.js
@@ -52,13 +52,31 @@
 
 // Examples:
 
+function binarySearch(arr, value) {
+  let searchStart = 0;
+  let searchEnd = arr.length - 1;
+
+  while (searchEnd - searchStart >= 0) {
+    let center = Math.floor((searchStart + searchEnd) / 2);
+    if (value === arr[center]) {
+      return center;
+    } else if (value > arr[center]) {
+      searchStart = center + 1;
+    } else {
+      searchEnd = center - 1;
+    }
+  }
+
+  return -1;
+}
+
 const yellowPages = ['Apple Store', 'Bags Galore', 'Bike Store', 'Donuts R Us', 'Eat a Lot', 'Good Food', 'Pasta Place', 'Pizzeria', 'Tiki Lounge', 'Zooper'];
-binarySearch(yellowPages, 'Pizzeria');                   // 7
-binarySearch(yellowPages, 'Apple Store');                // 0
+console.log(binarySearch(yellowPages, 'Pizzeria'));                   // 7
+console.log(binarySearch(yellowPages, 'Apple Store'));                // 0
 
-binarySearch([1, 5, 7, 11, 23, 45, 65, 89, 102], 77);    // -1
-binarySearch([1, 5, 7, 11, 23, 45, 65, 89, 102], 89);    // 7
-binarySearch([1, 5, 7, 11, 23, 45, 65, 89, 102], 5);     // 1
+console.log(binarySearch([1, 5, 7, 11, 23, 45, 65, 89, 102], 77));    // -1
+console.log(binarySearch([1, 5, 7, 11, 23, 45, 65, 89, 102], 89));    // 7
+console.log(binarySearch([1, 5, 7, 11, 23, 45, 65, 89, 102], 5));     // 1
 
-binarySearch(['Alice', 'Bonnie', 'Kim', 'Pete', 'Rachel', 'Sue', 'Tyler'], 'Peter');  // -1
-binarySearch(['Alice', 'Bonnie', 'Kim', 'Pete', 'Rachel', 'Sue', 'Tyler'], 'Tyler');  // 6
+console.log(binarySearch(['Alice', 'Bonnie', 'Kim', 'Pete', 'Rachel', 'Sue', 'Tyler'], 'Peter'));  // -1
+console.log(binarySearch(['Alice', 'Bonnie', 'Kim', 'Pete', 'Rachel', 'Sue', 'Tyler'], 'Tyler'));  // 6
